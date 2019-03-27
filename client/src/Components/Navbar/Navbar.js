08c1067b0bd53c839119c50f01b36b72023b5c47
SteiniSaem <thorsteinns16@ru.is>
2019-03-27 12:07:03 +0000

    stuuuuffff

---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <nav className="navigation-bar">
+      <img id="logo" src="./logo.png" />
+    </nav>
+  );
+};
+
+export default NavigationBar;
